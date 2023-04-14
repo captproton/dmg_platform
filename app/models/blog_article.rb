1a0b2005457173e3adc9fed5c0e78cbd21c5e215
captproton <carl@wdwhub.net>
2023-04-14 12:12:06 -0700

    CHANGE: updated layout of the show page

--- app/models/blog_article.rb
+++ app/models/blog_article.rb
@@ -9,12 +9,11 @@ class BlogArticle < ApplicationRecord
 
   has_one_attached :cover_photo
   
-  ##########################
-  # => Methods
-  ##########################
+
   def reading_time
     # https://alexanderpaterson.com/posts/showing-estimated-reading-time-on-a-rails-blog-post
-    text =  self.body.to_plain_text
+    text =  self.content.to_plain_text
     result = (text.scan(/\w+/).length / WORDS_PER_MINUTE).to_i ||= 1
-  end  
+  end
+
 end
