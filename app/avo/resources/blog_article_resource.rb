5c3b953984461a16d171b1ea0fe24275aae934b0
captproton <carl@wdwhub.net>
2023-04-23 16:29:04 -0700

    ADD: add tagging to avo

--- app/avo/resources/blog_article_resource.rb
+++ app/avo/resources/blog_article_resource.rb
@@ -14,11 +14,6 @@ class BlogArticleResource < Avo::BaseResource
   # Fields generated from the model
   field :title, as: :text
   # add fields here
-  field :tags,
-    as: :tags,
-    close_on_select: true,
-    placeholder: "Add some tags"
-
   field :pin_number, as: :number
   field :published_at,
     as: :date_time,
@@ -28,6 +23,11 @@ class BlogArticleResource < Avo::BaseResource
 
   field :content, as: :trix
   field :cover_photo, as: :file, is_image: true, link_to_resource: true
+  field :tags,
+    as: :tags,
+    acts_as_taggable_on: :tags,
+    close_on_select: true,
+    placeholder: "Add some tags"
 
 
   # field :user, as: :references, default: -> { current_user.id }, disabled: true
