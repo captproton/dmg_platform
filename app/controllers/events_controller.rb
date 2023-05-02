e6790605b71c8364c1fa901651ae3244069ed91e
captproton <carl@wdwhub.net>
2023-05-02 14:23:10 -0700

    FIX: form on events index didn't save

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
     def index
+      @lead = Lead.new
       @blog_articles    = BlogArticle.published
       @upcoming_events  = Event.upcoming_events
       mimimum_length_of_unique_youtube_id = 3
