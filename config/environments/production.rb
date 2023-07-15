24f66a679a1fcc84632bef25da5accb4d0af6657
Carl Tanner <hello@gocarlgo.com>
2023-07-15 10:52:41 -0700

    ADD: postmark app api key set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   #   enable_starttls_auto: true
   # }
   config.action_mailer.postmark_settings = {
-    api_token: Rails.application.credentials.postmark_api_token
+    api_token: ENV["POSTMARK"] #Rails.application.credentials.postmark_api_token
   }  
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
