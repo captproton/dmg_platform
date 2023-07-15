baaa3bc3788842551e9e00d976a6bade64591494
Carl Tanner <hello@gocarlgo.com>
2023-07-15 14:38:22 -0700

    FIX: attempt to undo error on server by changing mailer

--- app/mailers/lead_notification_mailer.rb
+++ app/mailers/lead_notification_mailer.rb
@@ -1,15 +1,8 @@
 class LeadNotificationMailer < ApplicationMailer
     def lead_notification(*args)
-        # array of emails to send to
-        support_team = User.support_team << "carl@wdwhub.net"
         puts "lead notification args: #{args}"
         # @user = params[:user]
         @lead = params[:lead]
-        mail(
-            to: support_team, 
-            subject: 'You have a new lead',
-            track_opens: 'true',
-            message_stream: 'outbound'
-            )
+        mail(to: "mike@dicemediagroup.com", subject: 'You have a new lead')
     end
 end
